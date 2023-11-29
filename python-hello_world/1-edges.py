b5668555afa4afe860cdae98d7072e457cc2f22f
Abdihakim <h_abdihakimi@outlook.com>
2023-11-29 09:48:53 +0300

    Finished edges file

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+
+print("First 3 letters: {}".format(word[0:3]+'\n'))
+
+print("Last 2 letters: {}".format(word[-2:]+'\n'))
+
+print("Middle word: {}".format(word[1:8]))
